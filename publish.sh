360b18410e9af72f892f4c32464fe4609710d55f
SimKDT <simon.cadet79@hotmail.fr>
2026-03-11 21:14:09 +0100

    publishing script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+set -euo pipefail
+
+UPDATE_TYPE="${1:-patch}"
+
+# publish to registries (VS Code Marketplace and Open VSX)
+vsce publish "$UPDATE_TYPE"
+npx ovsx publish
+
+# get version from package.json
+VERSION=$(node -e "console.log(require('./package.json').version)")
+
+# release to GitHub
+vsce package
+gh release create "v$VERSION" "project-zomboid-scripts-$VERSION.vsix" --notes "See [Changelog](https://github.com/SirDoggyJvla/ZedScripts/blob/main/README.md#Changelog) for details"
